49ad54b506dc8e7d6ff8d4590bbe7944554dab2b
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-22 12:22:29 -0500

    [RESTRUTURED]

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import reportWebVitals from './utils/vercel/reportWebVitals';
+import { sendToVercelAnalytics } from './utils/vercel/vitals';
+
+import Home from './pages/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
