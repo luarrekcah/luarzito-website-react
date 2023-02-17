a8a3e014c6f94987ae923822ba9a846e345eb573
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-16 21:43:23 -0500

    [added] pop and tos

--- src/index.js
+++ src/index.js
@@ -11,12 +11,16 @@ import reportWebVitals from './utils/vercel/reportWebVitals';
 import { sendToVercelAnalytics } from './utils/vercel/vitals';
 
 import Home from './pages/Home';
+import PolicyPrivacy from './pages/Pop';
+import TermsOfService from './pages/Tos';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/pop" element={<PolicyPrivacy />} />
+        <Route path="/tos" element={<TermsOfService />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
